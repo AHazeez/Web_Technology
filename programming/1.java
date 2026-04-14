67dd17cce923e67a9066c4ada071ce31b76124ac
Ahazeez <abdulhazeezkepu@gmail.com>
2026-04-14 18:07:22 +0530

    Added new files and updated code

--- programming/1.java
+++ programming/1.java
@@ -1,13 +1,13 @@
 class Pattern 
 {
     public static void main(String[] args){
-        int n = 10;
-        for(int i = 1; i <= n;i=i+2){
-            for(int j = i; j <=n-1; j++){
+        int n = 5;
+        for(int i = n; i >=1;i--){
+            for(int j = i; j <=n; j++){
                 System.out.print(" ");
             }
-            for(int k = 1; k <= i; k++){
-                System.out.print("* ");
+            for(int k = 1; k <=n*2-1; k++){
+                System.out.print(i+"  ");
        
             }
             System.out.println();
